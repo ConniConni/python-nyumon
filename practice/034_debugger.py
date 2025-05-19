cfa0a2175e7bff8b1e48a08b054ac281fed60725
ConniConni <nmtgktmo@gmail.com>
2025-05-19 18:20:43 +0900

    debbuger-2

--- practice/034_debugger.py
+++ practice/034_debugger.py
@@ -9,3 +9,18 @@ elif x == 10:
     print('10')
 else:
     print('positive')
+
+a = 5
+b = 10
+
+if a > 0:
+    print('a is positive')
+    a = str(a)
+    print(type(a),type(b))
+    if b > 0:
+        print('b is positive')
+        b += 1
+        print(b)
+
+
+
