c641761054eb9b46edde801b07238fd49bf2058f
ConniConni <nmtgktmo@gmail.com>
2025-05-19 16:10:07 +0900

    printで出力

---
+++ practice/09_input.py
@@ -0,0 +1,16 @@
+# printで出力
+
+# 改行される
+print('Hi')
+
+# 改行されない
+print('Hi',end='.')
+
+# ２改行される
+print('Hi', 'Mike', sep=',', end='.\n\n')
+
+# 改行されない
+print('Hi', 'Mike', sep=',', end='.')
+
+# 1行改行される
+print('Hi', 'Mike', sep=',', end='.\n')
