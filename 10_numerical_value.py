8d76ca448d873dd2b10548320e1ee4252494236c
ConniConni <nmtgktmo@gmail.com>
2025-05-19 16:23:55 +0900

    数値

---
+++ 10_numerical_value.py
@@ -0,0 +1,38 @@
+#数値計算
+
+import math
+#数学関数
+result = math.sqrt(25)
+print(result)
+
+y = math.log2(10)
+print(y)
+
+# ヘルプ参照
+print(help(math))
+
+
+#四則演算
+print(2+2, 5-2, 5*2, 10/2)
+
+# type
+print(type(1),type(1.6))
+
+# 割り算の商を求める
+print (17 // 3)
+
+# 割り算の余りを求める
+print (17 % 3)
+
+# 冪乗
+print (5 ** 5, 5 * 5 * 5 *5 * 5)
+
+# 変数の四則演算
+x = 5
+y = 2
+print(x + y, x -y, x * y, x / y, x // y, x % y, y ** x)
+
+# 丸め込み誤差
+pie = 3.14151515151151
+print(round(pie,2))
+
