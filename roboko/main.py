ad7386e767a9e3dfac89bac23bade61f5329fb8b
ConniConni <nmtgktmo@gmail.com>
2025-05-19 21:43:24 +0900

    Robokoのプロンプトを入力

---
+++ roboko/main.py
@@ -0,0 +1,13 @@
+print('こんにちは!私はRobokoです。あなたの名前は何ですか？')
+print('Hello, I am Roboko. What is your name?')
+
+user_name = input()
+
+print(user_name, 'さん。どこのレストランが好きですか？')
+print(user_name, 'which restaurants do you like?')
+
+favorite_restaurant = input()
+
+print('Roboko:',user_name, 'さん。ありがとうとございました。')
+print('Roboko:', 'Thank you so much',user_name,'!')
+
