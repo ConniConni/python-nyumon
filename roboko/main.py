5ba93742572eeba683ed22c97d01bfca802122dd
ConniConni <nmtgktmo@gmail.com>
2025-05-20 17:31:17 +0900

    引数を与えてmクラスのインスタンスを生成するように変更

--- roboko/main.py
+++ roboko/main.py
@@ -9,43 +9,47 @@ def frame(func):
 
 
 class Greeting(object):
-    def __init__(self):
+    def __init__(self, robot_name):
+        self.robot_name = robot_name
         cprint('####Greetingクラスのオブジェクトを初期化####', 'yellow')
     @frame
-    def hello(self, robot_name):
-        cprint(f'こんにちは!私は{robot_name}です。あなたの名前は何ですか？', 'green')
-        cprint(f'Hello, I am {robot_name}. What is your name?','green')
+    def hello(self):
+        cprint(f'こんにちは!私は{self.robot_name}です。あなたの名前は何ですか？', 'green')
+        cprint(f'Hello, I am {self.robot_name}. What is your name?','green')
 
 class FavoriteRestaurant(object):
-    def __init__(self):
+    def __init__(self, user_name):
+        self.user_name = user_name
         cprint('####FavoriteRestaurantクラスのオブジェクトを初期化####', 'yellow')
-    
+
     @frame
-    def question(self, user_name):
+    def question(self):
         cprint(f'{user_name}さん。どこのレストランが好きですか？', 'green')
-        cprint(f'{user_name}which restaurants do you like?', 'green')
+        cprint(f'{self.user_name}which restaurants do you like?', 'green')
 
 class GoodBye(object):
-    def __init__(self):
-         cprint('####GoodByeクラスのオブジェクトを初期化####', 'yellow')
-    
+    def __init__(self, robot_name, user_name):
+        self.robot_name = robot_name
+        self.user_name = user_name
+        cprint('####GoodByeクラスのオブジェクトを初期化####', 'yellow')
+
     @frame
-    def see_you(self, robot_name, user_name):
-        cprint(f'{robot_name}: {user_name}さん。ありがとうとございました。', 'green')
-        cprint(f'{robot_name}: Thank you so much {user_name}!', 'green')
+    def see_you(self):
+        cprint(f'{self.robot_name}: {self.user_name}さん。ありがとうとございました。', 'green')
+        cprint(f'{self.robot_name}: Thank you so much {self.user_name}!', 'green')
 
-greeting = Greeting()
+greeting = Greeting('Roboko')
 
-greeting.hello('Roboko')
+greeting.hello()
 
 user_name = input()
 
-favorite_restaurant = FavoriteRestaurant()
-favorite_restaurant.question(user_name)
+favorite_restaurant = FavoriteRestaurant(user_name)
+favorite_restaurant.question()
 
 favorite_restaurant = input()
 
-good_bye = GoodBye()
-good_bye.see_you('Roboko',user_name)
+good_bye = GoodBye('Roboko',user_name)
+good_bye.see_you()
 
 
