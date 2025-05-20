897667e54340ced0eb1455535a2de737aee5f7a9
ConniConni <nmtgktmo@gmail.com>
2025-05-20 14:32:08 +0900

    import

---
+++ practice/lesson.py
@@ -0,0 +1,6 @@
+# import lesson_package
+from lesson_package import utils
+
+# lesson_package.utils.say_hello('Jun')
+utils.say_hello('Jun')
+print('finish')
---
+++ practice/lesson_package/utils.py
@@ -0,0 +1,2 @@
+def say_hello(word):
+    print((word + '!')* 2)
