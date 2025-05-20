e8e802b8e30b9bafdb8333c3061275e04f8fc33b
ConniConni <nmtgktmo@gmail.com>
2025-05-20 16:54:34 +0900

    挨拶をクラス化

--- roboko/main.py
+++ roboko/main.py
@@ -1,5 +1,25 @@
-print('こんにちは!私はRobokoです。あなたの名前は何ですか？')
-print('Hello, I am Roboko. What is your name?')
+from termcolor import cprint
+
+def frame(func):
+    def wrapper(*args, **kwargs):
+        cprint('*' * 60, 'green')
+        func(*args, **kwargs)
+        cprint('*' * 60, 'green')
+    return wrapper
+
+
+class Greeting(object):
+    def __init__(self):
+        cprint('####Greetingクラスのオブジェクトを初期化####', 'yellow')
+    @frame
+    def hello(self, name):
+        cprint(f'こんにちは!私は{name}です。あなたの名前は何ですか？', 'green')
+        cprint(f'Hello, I am {name}. What is your name?','green')
+
+
+greeting = Greeting()
+
+greeting.hello('Roboko')
 
 user_name = input()
 
