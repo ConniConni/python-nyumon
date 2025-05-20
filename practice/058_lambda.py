a1ded87c377b1fc6b7f30bb89075e2ab42c9ea85
ConniConni <nmtgktmo@gmail.com>
2025-05-20 11:54:23 +0900

    lambda

--- practice/058_lambda.py
+++ practice/058_lambda.py
@@ -4,11 +4,12 @@ def change_words(words, func):
     for word in words:
         print(func(word))
 
+# 関数を定義し、実行
 def sample_func(word):
     return word.capitalize()
-
 change_words(l, sample_func)
 
 # lambdaを（無名関数）使って簡単にかく
+# ２行削減できる
 sample_func = change_words(l, lambda word: word.capitalize())
 
