85be0bebb8893f6673f579e1fcdff79e6b8b4974
ConniConni <nmtgktmo@gmail.com>
2025-05-22 19:27:06 +0900

    doctet作成

--- practice/161.doctest.py
+++ practice/161.doctest.py
@@ -2,9 +2,17 @@ class Cal():
     def add_num_and_double(self, x ,y):
         """ add and double
         >>> c = Cal()
-        >>> c.add_num_and_double(1,1)
+        >>> c.add_num_and_double(1, 1)
         4
+
+        >>> c.add_num_and_double('1', '1')
+        Traceback (most recent call last):
+        ...
+        TypeError
         """
+        # テスト結果から引数はintで受け取る必要があるとわかるため条件を追加
+        if type(x) is not int or type(y) is not int:
+            raise TypeError
         result = x + y
         result *= 2
         return result
@@ -14,5 +22,5 @@ if __name__ == '__main__':
     doctest.testmod()
 
     # cal = Cal()
-    # r = cal.add_num_and_double(1,1)
+    # r = cal.add_num_and_double(1,'1')
     # print(r)
