d889ef81cb5c0247c88f9947b5e12d1bbda0c1a5
ConniConni <nmtgktmo@gmail.com>
2025-05-22 19:18:08 +0900

    doctest

--- practice/161.doctest.py
+++ practice/161.doctest.py
@@ -1,10 +1,18 @@
 class Cal():
     def add_num_and_double(self, x ,y):
+        """ add and double
+        >>> c = Cal()
+        >>> c.add_num_and_double(1,1)
+        4
+        """
         result = x + y
         result *= 2
         return result
 
 if __name__ == '__main__':
-    cal = Cal()
-    r = cal.add_num_and_double(1,1)
-    print(r)
+    import doctest
+    doctest.testmod()
+
+    # cal = Cal()
+    # r = cal.add_num_and_double(1,1)
+    # print(r)
