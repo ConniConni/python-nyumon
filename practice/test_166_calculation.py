774bdf12ff97e0992db7be811439a13cb6c54397
ConniConni <nmtgktmo@gmail.com>
2025-05-23 15:13:00 +0900

    pytest　テスト前の処理

--- practice/test_166_calculation.py
+++ practice/test_166_calculation.py
@@ -6,15 +6,20 @@ import calculation
 def test_add_num_and_double():
     cal = calculation.Cal()
     assert cal.add_num_and_double(1,1) == 4
-
+1
 # テストをクラスで書く
 class TestCal(object):
+    # テスト前の処理を記載
+    def setup_method(self,method):
+        print(f'method={method.__name__}')
+        self.cal = calculation.Cal()
+
     def test_dd_num_and_double(self):
-        cal = calculation.Cal()
-        assert cal.add_num_and_double(1,1) == 4
+        # cal = calculation.Cal()
+        assert self.cal.add_num_and_double(1,1) == 4
 
     # 例外のテスト
     def test_add_num_and_double_raise(self):
         with pytest.raises(TypeError):
-            cal = calculation.Cal()
-            cal.add_num_and_double('1', '1')
+            # cal = calculation.Cal()
+            self.cal.add_num_and_double('1', '1')
