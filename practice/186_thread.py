61e4542fd0b418f1dcfae8bd1daadbd41e573efc
ConniConni <nmtgktmo@gmail.com>
2025-11-17 20:08:49 +0900

    スレッドのライフサイクルをかくにん

--- practice/186_thread.py
+++ practice/186_thread.py
@@ -1,11 +1,13 @@
 # スレッドを２つ立ててみる
 import threading
+import time
 
 
 # １つ目のスレッドで実行する処理の中身
 def worker1():
     # 現在実行中のスレッドの名前を取得し、start（end）と表示する
     print(threading.current_thread().name, "start")
+    time.sleep(3)
     print(threading.current_thread().name, "end")
 
 
@@ -13,6 +15,7 @@ def worker1():
 def worker2():
     # 現在実行中のスレッドの名前を取得し、start（end）と表示する
     print(threading.current_thread().name, "start")
+    time.sleep(4)
     print(threading.current_thread().name, "end")
 
 
