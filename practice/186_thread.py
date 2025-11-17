935efda5b86478a60714bb3dc98be942bda50dd1
ConniConni <nmtgktmo@gmail.com>
2025-11-17 20:29:11 +0900

    スレッドにargsを渡す

--- practice/186_thread.py
+++ practice/186_thread.py
@@ -1,6 +1,7 @@
 # 1. スレッドを２つ立ててみる
 # 2. loggingでスレッドの名前を出力する
 # 3. スレッドの名前を変える
+# 4. スレッド内で実行する関数に引数を渡す
 import logging
 import threading
 import time
@@ -19,10 +20,12 @@ def worker1():
 
 
 # 2つ目のスレッドで実行する処理の中身
-def worker2():
+def worker2(x, y=1):
     # 現在実行中のスレッドの名前を取得し、start（end）と表示する
     # print(threading.current_thread().name, "start")
     logging.debug("start")
+    logging.debug(x)
+    logging.debug(y)
     time.sleep(4)
     # print(threading.current_thread().name, "end")
     logging.debug("end")
@@ -31,7 +34,7 @@ def worker2():
 if __name__ == "__main__":
     # 変数にスレッドオブジェクトを代入
     t1 = threading.Thread(name="rename worker1", target=worker1)
-    t2 = threading.Thread(target=worker2)
+    t2 = threading.Thread(target=worker2, args=(10,), kwargs={"y": 100})
     # スレッド t1 を開始　メインプログラムの流れとは別にworker1関数の実行が始まる
     t1.start()
     t2.start()
