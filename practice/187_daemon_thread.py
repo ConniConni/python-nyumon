c1bcb0bb7ff983898b1947f1729857e216b73b5b
ConniConni <nmtgktmo@gmail.com>
2025-11-17 21:49:59 +0900

    セマフォを使って２つだけ占有できるようよにする

--- practice/187_daemon_thread.py
+++ practice/187_daemon_thread.py
@@ -1,3 +1,4 @@
+# セマフォを使って２つのスレッドが占有可能な状態にする
 import logging
 import threading
 import time
@@ -6,34 +7,34 @@ logging.basicConfig(level=logging.DEBUG, format="%(threadName)s: %(message)s")
 
 
 # １つ目のスレッドで実行する処理の中身
-def worker1(lock):
-    with lock:
+def worker1(semaphore):
+    with semaphore:
         logging.debug("start")
         time.sleep(3)
         logging.debug("end")
 
 
 # 2つ目のスレッドで実行する処理の中身
-def worker2(lock):
-    with lock:
+def worker2(semaphore):
+    with semaphore:
         logging.debug("start")
         time.sleep(3)
         logging.debug("end")
 
 
 # 3つ目のスレッドで実行する処理の中身
-def worker3(lock):
-    with lock:
+def worker3(semaphore):
+    with semaphore:
         logging.debug("start")
         time.sleep(3)
         logging.debug("end")
 
 
 if __name__ == "__main__":
-    lock = threading.RLock()
-    t1 = threading.Thread(target=worker1, args=(lock,))
-    t2 = threading.Thread(target=worker2, args=(lock,))
-    t3 = threading.Thread(target=worker3, args=(lock,))
+    semaphore = threading.Semaphore(2)
+    t1 = threading.Thread(target=worker1, args=(semaphore,))
+    t2 = threading.Thread(target=worker2, args=(semaphore,))
+    t3 = threading.Thread(target=worker3, args=(semaphore,))
     t1.start()
     t2.start()
     t3.start()
