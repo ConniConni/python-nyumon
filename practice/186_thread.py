586c64f276fecf853f3635b8413479b8220a5eee
ConniConni <nmtgktmo@gmail.com>
2025-11-17 20:06:54 +0900

    threadをたてる

---
+++ practice/186_thread.py
@@ -0,0 +1,26 @@
+# スレッドを２つ立ててみる
+import threading
+
+
+# １つ目のスレッドで実行する処理の中身
+def worker1():
+    # 現在実行中のスレッドの名前を取得し、start（end）と表示する
+    print(threading.current_thread().name, "start")
+    print(threading.current_thread().name, "end")
+
+
+# 2つ目のスレッドで実行する処理の中身
+def worker2():
+    # 現在実行中のスレッドの名前を取得し、start（end）と表示する
+    print(threading.current_thread().name, "start")
+    print(threading.current_thread().name, "end")
+
+
+if __name__ == "__main__":
+    # 変数にスレッドオブジェクトを代入
+    t1 = threading.Thread(target=worker1)
+    t2 = threading.Thread(target=worker2)
+    # スレッド t1 を開始　メインプログラムの流れとは別にworker1関数の実行が始まる
+    t1.start()
+    t2.start()
+    print("started")
