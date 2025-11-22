81041605353945974aa87659dc170d522cba73da
ConniConni <nmtgktmo@gmail.com>
2025-11-22 14:47:52 +0900

    関数化

--- practice/208_hash.py
+++ practice/208_hash.py
@@ -4,12 +4,18 @@ user_name = "user1"
 user_pass = "password"
 db = {}
 
-# user_passをutf-8でbytes表記に変換
-password = bytes(user_pass, "utf-8")
-# password = user_pass.encode("utf-8") も同じ挙動になる
 
-# passwordをハッシュ化
-digest = hashlib.sha256(password).hexdigest()
+def get_digest(password):
+    # user_passをutf-8でbytes表記に変換
+    password = bytes(password, "utf-8")
+    # password = user_pass.encode("utf-8") も同じ挙動になる
+
+    # passwordをハッシュ化
+    digest = hashlib.sha256(password).hexdigest()
+    return digest
+
+
 # dbに値を追加
-db[user_name] = digest
+db[user_name] = get_digest(user_pass)
+
 print(db)
